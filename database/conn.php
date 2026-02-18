c9338bfd3a20118ab849715d450d0d2a2168151e
Seu Nome <seu-email@gmail.com>
2026-02-18 16:12:42 -0300

    Feat Conexão com Banco

---
+++ database/conn.php
@@ -0,0 +1,12 @@
+<?php
+
+$hostname= 'localhost';
+$database= 'to_do_list';
+$username = 'postgress';
+$password = '1234';
+
+try {
+$pdo = new PDO("pgsql:host=$hostmane;dbname=$database", $username, $password);
+} catch (PDOEexception $e) {
+    echo "Erro: " . $e->getMessage(); 
+}
